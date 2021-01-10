91a7be8f8c534b00f5ec1c5a2dfae791a5ae2a39
ll_admin <manikanteshiosdev@gmail.com>
2021-01-10 02:37:27 -0600

    Added SWRevealViewController files and created bridging header in supporting files

---
+++ Text-Em/Controller/ChannelVC.swift
@@ -0,0 +1,30 @@
+//
+//  ChannelVC.swift
+//  Text-Em
+//
+//  Created by Mani V on 1/10/21.
+//  Copyright © 2021 iOS-Apps. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Text-Em/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Text-Em
+//
+//  Created by Mani V on 1/10/21.
+//  Copyright © 2021 iOS-Apps. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    @IBOutlet var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+}
